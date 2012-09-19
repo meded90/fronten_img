df3565d70b062822d035c4cdc6e52636e7ffb628
meded90 <meded90@gmail.com>
2012-09-19 18:42:57 +0600

    добовление из кортинки

---
+++ fronendImg.js
@@ -0,0 +1,10 @@
+
+var gineretTestImg =function ($this) {
+//    var elements = document.getElementsBySelector("img")
+//    console.log(elements);
+
+    console.log($this);
+}
+//gineretTestImg("ds");
+
+
