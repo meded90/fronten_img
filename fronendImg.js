f92c4d110414b61ef3e6b26720177aa6a6119344
meded90 <meded90@gmail.com>
2012-09-21 11:41:22 +0600

    ишим все для генерации

--- fronendImg.js
+++ fronendImg.js
@@ -1,10 +1,13 @@
 
-var gineretTestImg =function ($this) {
-//    var elements = document.getElementsBySelector("img")
-//    console.log(elements);
+var gineretTestImg =function () {
+    var elements = document.getElementsBySelector("img.imgGeneretToFrontEnd")
+    for (var i = 0; i < elements.length; i++) {
+        var obj = elements[i];
+        console.log(obj);
+    }
 
-    console.log($this);
 }
-//gineretTestImg("ds");
-
-
+document.load(function(){
+    alet(1)
+    gineretTestImg()
+});
